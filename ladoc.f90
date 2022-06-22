c4e8ce4734a8e868e61e0e3249f1b65a10942015
LucasDubuc-GA <107855494+LucasDubuc-GA@users.noreply.github.com>
2022-06-22 20:16:09 +0200

    Add files via upload

--- ladoc.f90
+++ ladoc.f90
@@ -1,5 +1,5 @@
-!Programe réaliser par Lucas Dubuc
-!Ce programe crée une requette internet pour Edge 
+!Programme réaliser par Lucas Dubuc
+!Ce programme crée une requette internet pour Edge 
 
 program ladoc
 	!Déclaration des variables
@@ -7,17 +7,28 @@ program ladoc
 	character (len=50) :: arg
 	integer :: i
 	
-	
-	req = "start msedge www.bing.com/search?q="
+	! On déclare la string de requête
+	req = "start msedge "  
 	i = 1
+	
 	call get_command_argument(i, arg)
-	do while(arg /= "") ! /= est le remplacent de !=
-		req = trim(req) // arg
-		req = trim(req) // "+"
+	if(arg == "url") then ! sinon on cherche l'URL
+		req = trim(req) // " " // arg
+	else if(arg == "localhost") then
+		req = "start msedge http://localhost/"
+	else ! si on veut faire une recherche par mot clés 
+		req = trim(req) // " www.bing.com/search?q="
+		do while(arg /= "") ! /= est le remplacent de !=
+			req = trim(req) // arg
+			req = trim(req) // "+"
 		
-		i = i + 1
-		call get_command_argument(i, arg)
-	end do
+			i = i + 1
+			call get_command_argument(i, arg)
+		end do
 	!L'avantage sur le batch est que nous ne somme pas limiter à 9 mots
+	end if
+	
+	
+	write(*, '(a)') req
 	call system(req)
 end program
